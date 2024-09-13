bf65d53d958399b5c89db170aa235f4c12ede694
BijayX <sthabeejay2060@gmail.com>
2024-09-13 22:31:40 +0545

    my-protfolios

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
+
 import { SectionHeading } from '@/components/section-heading';
 import { Skills } from '@/components/skills';
 import { useSectionInView } from '@/hooks/use-section-in-view';
@@ -14,11 +15,13 @@ export const About = () => {
       className="my-10 flex w-full scroll-mt-28 flex-col items-center"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}>
-      <SectionHeading heading="About Me"/>
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading heading="About Me" />
       <div className="-mt-5 max-w-2xl text-center leading-7">
         <p className="mb-4">
-          I&apos;m Bijay Kumar Shrestha, a passionate software developer from Nepal. I have developed a strong affinity for both front-end and
+          I&apos;m Bijay Kumar Shrestha, a passionate software developer from
+          Nepal. I have developed a strong affinity for both front-end and
           back-end development. I thrive on the problem-solving aspect of
           programming, relishing the satisfaction that comes with finding
           solutions to complex challenges. I&apos;ve had the opportunity to work
@@ -36,7 +39,7 @@ export const About = () => {
           experience then don&apos;t hesitate to contact me.
         </p>
       </div>
-      <Skills/>
+      <Skills />
     </motion.section>
   );
 };
